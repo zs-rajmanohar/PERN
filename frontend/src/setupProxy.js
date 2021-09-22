8982d431a7a2326e6c82c1daa08b8ed3a320d4ca
naveen <naveen@gmail.com>
2021-09-22 16:38:05 +0530

    PERN

--- frontend/src/setupProxy.js
+++ frontend/src/setupProxy.js
@@ -4,6 +4,6 @@ const proxy = require('http-proxy-middleware');
 
 module.exports = function(app) {
 
-  app.use(proxy('/api/customers', { target: url }))
+  app.use(proxy('/api/customer', { target: url }))
 
 }
